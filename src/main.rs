ad9ff293fb3d301e9eec316efa8635ecfe8dfe09
matt raffel <matt.raffel@evernym.com>
2018-06-14 13:23:24 -0600

    no message

--- src/main.rs
+++ src/main.rs
@@ -28,19 +28,36 @@ use input::from_stdin::StdReader;
 use processors::process_for_individual_test_results::ProcessIndividualTestResults;
 use utils::logger::init_log;
 
+
+fn print_help() {
+    println!();
+    println!("{}", Color::Green.paint("rust-test-parser"));
+    println!("\t by me");
+    println!();
+    println!("usage is pretty simple, assuming rust-test-parser is in your path. call it like this,");
+    println!("in the same directory you ran {}:", Color::White.paint("cargo build"));
+    println!("{}", Color::Blue.paint("\tcargo test | rust-test-parser"));
+    println!();
+}
+
 fn main() {
     init_log();
 
     debug!("rust-test-parser has started ...");
+    if env::args().find(|a| a == "-h" || a == "--help").is_some() {
+        return print_help();
+    }
 
     let results: Vec<String> = StdReader::read_all();
     let results: Vec<String> = ProcessIndividualTestResults::find_test_lines(&results);
 
-    println!("---------------------");
-    println!("{:?}", results);
+    trace!("------- after processing input --------------");
+    trace!("{:?}", results);
 
     let organized_results = ProcessIndividualTestResults::group_test_results(&results);
 
     organized_results.print();
 
 }
+
+
