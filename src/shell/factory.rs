d27051b6df097759177e388bb747908cdb6b67b6
matt raffel <matt.raffel@evernym.com>
2018-06-27 12:42:54 -0600

    no message

--- src/shell/factory.rs
+++ src/shell/factory.rs
@@ -1,15 +1,27 @@
-//!
+//! ShellTypes are the handlers of IO for properly display formatted test results
+//! ShellTypes implement ShellTrait and open their display
 
 use shell::shell_trait::ShellTrait;
 use shell::browser::Browser;
 use shell::console::Console;
 
+/**
+    ShellTypes enum
+*/
 pub enum ShellTypes {
+    /**
+        represents the standard console
+    */
     Console(Console),
+    /**
+        represents html display in a browser
+    */
     Browser(Browser),
 }
 
 impl ShellTrait for ShellTypes {
+    /**
+    */
     fn open(&self) {
         match *self {
             ShellTypes::Console(ref console) => console.open(),
