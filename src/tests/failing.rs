9112836038990693880b4eed07b2a0a51488e7af
matt raffel <matt.raffel@evernym.com>
2018-07-05 09:56:56 -0600

    no message

--- src/tests/failing.rs
+++ src/tests/failing.rs
@@ -1,10 +1,19 @@
 
+
+// this test is here to test fail output.  remove ignore attribute to make it fail
+// using ignore attribute so the CI passes because I can figure out how to conditionally
+// use it
 #[test]
+#[ignore]
 fn failing_one() {
     assert!(false, "this is a failing test");
 }
 
+// this test is here to test fail output.  remove ignore attribute to make it fail
+// using ignore attribute so the CI passes because I can figure out how to conditionally
+// use it
 #[test]
+#[ignore]
 fn failing_two()  {
     assert!(false, "this is a failing test");
 }
