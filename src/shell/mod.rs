974246a8445eeafbc0eadbd7cb08d9231eba8f92
matt raffel <matt.raffel@evernym.com>
2018-07-05 07:43:18 -0600

    no message

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -1,5 +1,7 @@
 //! shell module is responsible for displaying test results formatted from an OutputTrait implementation
-//! in the output mod
+//! in the output mod.  ShellTypes are related to OutputTypes in that it makes no sense to
+//! use console shell type when the output is html.  therefore OutputType implementations return
+//! which shell type to use
 
 pub mod browser;
 pub mod console;
