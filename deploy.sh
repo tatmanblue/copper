6ca7ded429c7a9c7c3954f978286fb2303725687
matt raffel <matt.raffel@evernym.com>
2018-08-02 13:08:11 -0600

    no message
    Signed-off-by: matt raffel <matt.raffel@evernym.com>

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+# this script is meant for developers needing to update their enviroment of changes
+# and not wanting to use symbolic links
+
 cp src/templates/index.html ~/.copper/templates/
 rm ~/.copper/results/*
