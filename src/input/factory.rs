665c5da4a80626a3508b03e8414a6d65afb0f42c
matt raffel <matt.raffel@evernym.com>
2018-08-02 13:47:20 -0600

    no message
    Signed-off-by: matt raffel <matt.raffel@evernym.com>

--- src/input/factory.rs
+++ src/input/factory.rs
@@ -1,15 +1,33 @@
+//! The input factory determines the source of input
+//! input is assumed to mean "cargo test output" since it is ready by copper
+//! how that input is received is determined by environment settings and command line
+//! overrides
 //!
+//! there's two sources:
+//!    STDIN  (default, use like 'cargo test | copper')
+//!    a file (use like 'cargo test > test.txt && copper -f test.txt')
 
 use input::from_file::FileReader;
 use input::from_stdin::StdReader;
 use input::input_trait::InputTrait;
 use utils::environment::Environment;
 
+/**
+    InputTypes enums becomes a "placeholder" (so to speak) of the concrete implementation of
+    InputTrait which handles the actual input reader.   This allows getting around "known size"
+    requirements rust has on return types.
+*/
 pub enum InputTypes {
     StdIn(StdReader),
     File(FileReader),
 }
 
+/**
+    Think of this InputTrait implementation as something along the lines of an abstract class v-table pointer of
+    in an OO language.   It is used to route function calls to specific implementations.
+
+    The concrete implementation are in their respective files.
+*/
 impl InputTrait for InputTypes {
     fn read_all(&self) -> Vec<String> {
         match *self {
